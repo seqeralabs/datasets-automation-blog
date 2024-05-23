5bbd5cc270e8e3439a1e1a224515e496a6e1b871
Adrian Navarro <adrian.navarro@seqera.io>
2024-05-23 17:28:59 +0200

    Increase tower version

--- entry_script.sh
+++ entry_script.sh
@@ -2,7 +2,7 @@
 
 # Reference: https://docs.aws.amazon.com/lambda/latest/dg/images-test.html
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-  exec /usr/local/bin/aws-lambda-rie-x86_64 /usr/bin/python3 -m awslambdaric "$@"
+  exec /usr/local/bin/aws-lambda-rie-x86_64 /usr/local/bin/python -m awslambdaric "$@"
 else
-  exec /usr/bin/python3 -m awslambdaric "$@"
+  exec /usr/local/bin/python -m awslambdaric "$@"
 fi
